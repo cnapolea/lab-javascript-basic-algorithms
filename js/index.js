34ea72016b8982471c34d79ea7fa152fdcd6e17e
cnapolea <cjnapoleao@gmail.com>
2021-06-05 14:52:40 +0100

    Feat: Complete Interation 2: \n - Declare variables to hold the length of each name (navigator and driver) \n - Write conditional statement to access which name is longer based on each name's characters

--- js/index.js
+++ js/index.js
@@ -10,5 +10,15 @@ let  hacker2 = 'Norah';
 console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
 
+let hacker1Length = hacker1.length;
+let hacker2Length = hacker2.length;
+
+if (hacker1Length > hacker2Length) {
+    console.log(`The driver has the logest name, it has ${hacker1Length} characters`);
+} else if (hacker1Length < hacker2Length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2Length} characters`);
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1Length} characters!`);
+}
 
 // Iteration 3: Loops
