3277e5824eae13c3286a0477677618d842c32d15
cnapolea <cjnapoleao@gmail.com>
2021-06-05 14:49:22 +0100

    Feat: Complete Interation 1: \n - Create variables to hold driver and navigator names \n - Print requested phrases

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
+// jshint esversion:10
 // Iteration 1: Names and Input
 
+let  hacker1 = 'Cristiano';
 
+console.log(`The driver's name's is ${hacker1}`);
+
+let  hacker2 = 'Norah';
+
+console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
 
 
